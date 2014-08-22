adf45f4fda2bfc102c231f29e494977fa7ba22ab
Agustin Marmolejo <eljamz@gmail.com>
2014-08-22 15:24:15 -0500

    Prevent Logging of Passwords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Prevent logging of Passwords
+  config.filter_parameters += [:password, :password_confirmation]
 end
